fe16b1fb0a31215ca3dbe822b82e3dc6170bf88b
wstern1234 <58192563+wstern1234@users.noreply.github.com>
2021-09-24 12:03:31 -0700

    Update C210-A5.cpp

--- Assignment 5/C210-A5.cpp
+++ Assignment 5/C210-A5.cpp
@@ -6,27 +6,36 @@ using namespace std;
 
 
 
-void TowersOfHanoi(int num, char source, char aux, char des)
+// calculates moves for disks
+void TowersOfHanoi(int num, char source, char aux, char dest)
 {
+  // if num is 1, move disk to dest
 	if (num == 1)
 	{
-		cout << "Move a disk from " << source << " to " << des << endl;
+		cout << "Move a disk from " << source << " to " << dest << endl;
 		return;
 	}
 
-	TowersOfHanoi(num - 1, source, des, aux);
-	cout << "Move a disk from " << source << " to " << des << endl;
-	TowersOfHanoi(num - 1, aux, source, des);
+  // 2 recursive choices, either source, dest, aux or aux, source, dest
+	TowersOfHanoi(num - 1, source, dest, aux);
+	cout << "Move a disk from " << source << " to " << dest << endl;
+	TowersOfHanoi(num - 1, aux, source, dest);
 }
+
+
+
+
 
 int main() {
 
+  // buffer for input, total number of disks
 	string buffer;
 	int numDisks;
 
 	cout << "Enter a number of disks to play: ";
 	cin >> buffer;
 
+  // converts string input to int
 	numDisks = stoi(buffer);
 
 	TowersOfHanoi(numDisks, 'A', 'B', 'C');
